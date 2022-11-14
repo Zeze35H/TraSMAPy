391bb57f05f2427533f5c6e8bae195b05509c20a
Ana Barros <up201806593@fe.up.pt>
2022-11-14 19:14:49 +0000

    Add: many edge getters

--- src/trasmapy/concessioner/_Edge.py
+++ src/trasmapy/concessioner/_Edge.py
@@ -20,6 +20,82 @@ class Edge(IdentifiedObject):
     def getLane(self, laneId):
         return self._lanes[laneId]
 
+    @property
+    def laneNumber(self):
+        return self._lanes.len;
+    
+    @property
+    def streetName(self): 
+        return traci.edge.getStreetName(self.id)
+
+    @property
+    def traveltime(self):
+        return traci.edge.getTraveltime(self.id)
+
+    @property
+    def CO2Emission(self):
+        return traci.edge.getCO2Emission(self.id)
+
+    @property
+    def COEmission(self):
+        return traci.edge.getCOEmission(self.id)
+    
+    @property
+    def HCEmission(self):
+        return traci.edge.getHCEmission(self.id)
+
+    @property
+    def PMxEmission(self):
+        return traci.edge.getPMxEmission(self.id)
+
+    @property
+    def NOxEmissions(self):
+        return traci.edge.getNOxEmission(self.id)
+
+    @property
+    def fuelConsumption(self):
+        return traci.edge.getFuelConsumption(self.id)
+
+    @property
+    def noiseEmission(self):
+        return traci.edge.getFuelConsumption(self.id)
+
+    @property
+    def electricityConsumption(self):
+        return traci.edge.getElectricityConsumption(self.id)
+
+    @property
+    def lastStepVehicleNumber(self):
+        return traci.edge.getLastStepVehicleNumber(self.id)
+
+    @property
+    def lastStepMeanSpeed(self):
+        return traci.edge.getLastStepMeanSpeed(self.id) 
+
+    @property
+    def lastStepVehicleIDs(self):
+        return traci.edge.getLastStepVehicleIDs(self.id)
+
+    @property
+    def lastStepOccupancy(self):
+        return traci.edge.getLastStepOccupancy(self.id)
+
+    @property
+    def lastStepLength(self):
+        return traci.edge.getLastStepLength(self.id)
+
+    @property
+    def waitingTime(self):
+        return traci.edge.getWaitingTime(self.id)
+
+    @property
+    def lastStepPersonIDs(self):
+        return traci.edge.getLastStepPersonIDs(self.id)
+
+    @property
+    def lastStepHaltingNumber(self):
+        return traci.edge.getLastStepHaltingNumber(self.id)
+
     def setMaxSpeed(self, maxSpeed: float) -> None:
         """Sets the maximum speed for the vehicles in this edge (for all lanes) to the given value."""
         # Can't use traci directly because Lane state needs to be updated: traci.edge.setMaxSpeed(self.id, maxSpeed)
