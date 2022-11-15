bfab8ce421556ecb9b0309bf86da876155db85cc
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-11-15 16:54:15 +0000

    Add rand network for load speed tests

---
+++ examples/rand/runner.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+
+from trasmapy import TraSMAPy
+
+
+def run(traSMAPy: TraSMAPy):
+    """execute the TraCI control loop"""
+    for i in range(0, 50):
+        traSMAPy.users.createVehicle(f"vehicle{i}", "")
+
+    while traSMAPy.minExpectedNumber > 0:
+        traSMAPy.doSimulationStep()
+
+    traSMAPy.closeSimulation()
+
+
+if __name__ == "__main__":
+    traSMAPy = TraSMAPy("rand.sumocfg")
+    run(traSMAPy)
--- examples/simple/runner.py
+++ examples/simple/runner.py
@@ -8,12 +8,7 @@ import traci
 
 def run(traSMAPy: TraSMAPy):
     """execute the TraCI control loop"""
-    print(traSMAPy.network.edges)
-    print(traSMAPy.network.lanes)
-    print(traSMAPy.network.stops)
-
-    laneId = "1to2_1"
-    lane = traSMAPy.network.getLane(laneId)
+    lane = traSMAPy.network.getLane("1to2_1")
     lane.setDisallowed([VehicleClass.PASSENGER])
 
     e10 = traSMAPy.network.getDetector("e1_0")
@@ -22,18 +17,13 @@ def run(traSMAPy: TraSMAPy):
     bus = traSMAPy.users.createVehicle("vehicle0", "route0", typeId="Bus")
     bus.stopFor("bs_0", 10, endPos=1, stopTypes=[StopType.BUS_STOP, StopType.PARKING])
     for i in range(1, 5):
-        traSMAPy.users.createVehicle(
-            f"vehicle{i}", "route0", typeId="Car", personNumber=5, personCapacity=10
-        )
-
+        traSMAPy.users.createVehicle(f"vehicle{i}", "route0", typeId="Car")
 
     #  print(traci.vehicle.getRouteID("vehicle0"))
-    #  print(traci.vehicle.getNextStops("vehicle0"))
 
     while traSMAPy.minExpectedNumber > 0:
         if traSMAPy.step > 20:
             lane.allowAll()
-
         traSMAPy.doSimulationStep()
 
     traSMAPy.closeSimulation()
