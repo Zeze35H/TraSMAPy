a05f3dcaf4365912138278aceca73ef398a1fd0c
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-11-15 21:04:11 +0000

    Add: pyflwor for querries

--- examples/simple/runner.py
+++ examples/simple/runner.py
@@ -4,6 +4,7 @@
 from trasmapy import TraSMAPy, VehicleClass, StopType
 
 import traci
+import pyflwor
 
 
 def run(traSMAPy: TraSMAPy):
@@ -15,11 +16,9 @@ def run(traSMAPy: TraSMAPy):
     e10.listen(lambda x: print(x))
 
     bus = traSMAPy.users.createVehicle("vehicle0", "route0", typeId="Bus")
-    bus.stopFor("bs_0", 10, endPos=1, stopTypes=[StopType.BUS_STOP, StopType.PARKING])
+    bus.stopFor("bs_0", 20, endPos=1, stopTypes=[StopType.BUS_STOP])
     for i in range(1, 5):
         traSMAPy.users.createVehicle(f"vehicle{i}", "route0", typeId="Car")
-
-    #  print(traci.vehicle.getRouteID("vehicle0"))
 
     while traSMAPy.minExpectedNumber > 0:
         if traSMAPy.step > 20:
@@ -31,4 +30,6 @@ def run(traSMAPy: TraSMAPy):
 
 if __name__ == "__main__":
     traSMAPy = TraSMAPy("hello.sumocfg")
-    run(traSMAPy)
+    #  run(traSMAPy)
+    q = pyflwor.compile("trasmapy/network/edges[self.id == '1to2']/stops")
+    print(q({"trasmapy": traSMAPy}))
