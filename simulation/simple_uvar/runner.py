dec449397a2c7e88af959befa6c992a9a524fcc9
Ivo Saavedra <ivo_saavedra@outlook.com>
2022-12-04 21:46:19 +0000

    Working example with stops

--- simulation/simple_uvar/runner.py
+++ simulation/simple_uvar/runner.py
@@ -1,36 +1,63 @@
 #!/usr/bin/env python
 
 
-from trasmapy import TraSMAPy, Color, VehicleClass
+from trasmapy import TraSMAPy, Color, VehicleClass, StopType
 
 
 
 def run(traSMAPy: TraSMAPy):
     """execute the TraCI control loop"""
-    
-    e40 = traSMAPy.network.getEdge("E40")
-    e53 = traSMAPy.network.getEdge("E53")
-    e48 = traSMAPy.network.getEdge("E48")
-    
-    
-    e41 = traSMAPy.network.getEdge("E41")
+
+
+    # vehicle types
     defaultVehicle = traSMAPy.users.getVehicleType("DEFAULT_VEHTYPE")
-    
     evehicleType = defaultVehicle.duplicate("evehicle")
     evehicleType.vehicleClass = VehicleClass.EVEHICLE
     evehicleType.color = Color(0, 0, 255)
-    e41.setDisallowed([defaultVehicle.vehicleClass])
-        
-    route0 = traSMAPy.users.getRoute('r_0')    
-    
-    route1 = traSMAPy.users.createRouteFromEdges("r_2", [e40, e53, e48])
 
-    for i in range(0, 300, 3):
-        traSMAPy.users.createVehicle(f"vehicle{i}", route0, defaultVehicle)
-        traSMAPy.users.createVehicle(f"vehicle{i+1}", route1, defaultVehicle)
-        traSMAPy.users.createVehicle(f"vehicle{i+2}", route1, evehicleType)
+    # get edges
+    e40 = traSMAPy.network.getEdge("E40")
+    e53 = traSMAPy.network.getEdge("E53")
+    e58 = traSMAPy.network.getEdge("E58")
+    e48 = traSMAPy.network.getEdge("E48")
+    e41 = traSMAPy.network.getEdge("E41")
+
+
+    e31 = traSMAPy.network.getEdge("E31")
+
+    
+    # forbid access to non eletric vehicles
+    e41.setDisallowed([defaultVehicle.vehicleClass])
+    # e45.setDisallowed([defaultVehicle.vehicleClass])
+
+    # get prehexisting routes 
+    route0 = traSMAPy.users.getRoute('r_0')  
+    route1 = traSMAPy.users.getRoute('r_1')  
+
+    # setup custom route
+    route2 = traSMAPy.users.createRouteFromEdges("r_2", [e40, e58])
+
+    stop0 = traSMAPy.network.createEdgeStop(edgeId=e31.id, startPos=10, endPos=10)
+    # lane0 = e53.getLane("E31_0")
+    # stop0 = traSMAPy.network.createLaneStop(lane0.id, endPos=0)
+
+    pa_0 = traSMAPy.network.getStop('pa_0')
+
+    v = traSMAPy.users.createVehicle(f"vehicle1", route2, defaultVehicle)
+    
+    # for i in range(0, 300, 3):
+    #     # traSMAPy.users.createVehicle(f"vehicle{i}", route0, defaultVehicle)
+    #     v0 = traSMAPy.users.createVehicle(f"vehicle{i+1}", route2, defaultVehicle)
+    #     v1 = traSMAPy.users.createVehicle(f"vehicle{i+2}", route2, evehicleType)
+
+    #     # v0.stopFor(stop0, 0)
+    #     # v1.stopFor(stop0, 0)
         
     while traSMAPy.minExpectedNumber > 0:
+
+        if traSMAPy.step == 10:
+            v.stopFor(stop0, 0.1)
+
         traSMAPy.doSimulationStep()
 
     traSMAPy.closeSimulation()
