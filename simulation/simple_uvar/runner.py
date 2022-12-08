d5cb8a33edb5a152d65018c2aa6638e41e066de6
Ivo Saavedra <ivo_saavedra@outlook.com>
2022-12-08 16:08:11 +0000

    Network redesign

--- simulation/simple_uvar/runner.py
+++ simulation/simple_uvar/runner.py
@@ -17,10 +17,14 @@ def run(traSMAPy: TraSMAPy):
 
     # get edges
     e40 = traSMAPy.network.getEdge("E40")
+    e9 = traSMAPy.network.getEdge("E9")
+
     e53 = traSMAPy.network.getEdge("E53")
     e58 = traSMAPy.network.getEdge("E58")
     e48 = traSMAPy.network.getEdge("E48")
+    
     e41 = traSMAPy.network.getEdge("E41")
+    e41a = traSMAPy.network.getEdge("-E41.28")
 
 
     e31 = traSMAPy.network.getEdge("E31")
@@ -28,14 +32,15 @@ def run(traSMAPy: TraSMAPy):
     
     # forbid access to non eletric vehicles
     e41.setDisallowed([defaultVehicle.vehicleClass])
+    e41a.setDisallowed([defaultVehicle.vehicleClass])
     # e45.setDisallowed([defaultVehicle.vehicleClass])
 
     # get prehexisting routes 
-    route0 = traSMAPy.users.getRoute('r_0')  
-    route1 = traSMAPy.users.getRoute('r_1')  
+    # route0 = traSMAPy.users.getRoute('r_0')  
+    # route1 = traSMAPy.users.getRoute('r_1')  
 
     # setup custom route
-    route2 = traSMAPy.users.createRouteFromEdges("r_2", [e40, e58])
+    route2 = traSMAPy.users.createRouteFromEdges("r_2", [e40, e9])
 
     stop0 = traSMAPy.network.createEdgeStop(edgeId=e31.id, startPos=10, endPos=10)
     # lane0 = e53.getLane("E31_0")
@@ -43,20 +48,20 @@ def run(traSMAPy: TraSMAPy):
 
     pa_0 = traSMAPy.network.getStop('pa_0')
 
-    v = traSMAPy.users.createVehicle(f"vehicle1", route2, defaultVehicle)
+    # v = traSMAPy.users.createVehicle(f"vehicle1", route2, defaultVehicle)
     
-    # for i in range(0, 300, 3):
-    #     # traSMAPy.users.createVehicle(f"vehicle{i}", route0, defaultVehicle)
-    #     v0 = traSMAPy.users.createVehicle(f"vehicle{i+1}", route2, defaultVehicle)
-    #     v1 = traSMAPy.users.createVehicle(f"vehicle{i+2}", route2, evehicleType)
+    vs = []
+    evs = []
+    for i in range(0, 300, 3):
+        # traSMAPy.users.createVehicle(f"vehicle{i}", route0, defaultVehicle)
+        vs.append(traSMAPy.users.createVehicle(f"vehicle{i+1}", route2, defaultVehicle))
+        evs.append(traSMAPy.users.createVehicle(f"vehicle{i+2}", route2, evehicleType))
 
-    #     # v0.stopFor(stop0, 0)
-    #     # v1.stopFor(stop0, 0)
         
     while traSMAPy.minExpectedNumber > 0:
 
-        if traSMAPy.step == 10:
-            v.stopFor(stop0, 0.1)
+        # if traSMAPy.step == 10:
+        #     v.stopFor(stop0, 0.1)
 
         traSMAPy.doSimulationStep()
 
