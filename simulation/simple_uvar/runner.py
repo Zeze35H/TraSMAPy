a4486161a880640566391b305b7dc9cdaf9d0149
Ivo Saavedra <ivo_saavedra@outlook.com>
2022-12-13 16:37:02 +0000

    Fix: removed north parking areas (not working)

--- simulation/simple_uvar/runner.py
+++ simulation/simple_uvar/runner.py
@@ -13,7 +13,6 @@ def create_route(r_id : str,  edges : list, r_type : str = "normal",
         "prob" : prob,
         "park_areas" : parks
     }  
-    
 
 def run(traSMAPy: TraSMAPy):
     """execute the TraCI control loop"""
@@ -32,7 +31,6 @@ def run(traSMAPy: TraSMAPy):
     busType.color = Color(255, 255, 0)
     busType.shape = "bus"
 
-
     # get edges
     e40 = traSMAPy.network.getEdge("E40")
     e40r = traSMAPy.network.getEdge("-E40")
@@ -50,20 +48,14 @@ def run(traSMAPy: TraSMAPy):
 
 
     PARKING_AREAS_SOUTH = [traSMAPy.network.getStop(f'pa_sw{x}') for x in range(7)]
-    PARKING_AREAS_NORTH = [traSMAPy.network.getStop(f"pa_ne{i}") for i in range(14)]
-
-    PARKS = PARKING_AREAS_SOUTH + PARKING_AREAS_NORTH
 
     V_TYPES = [defaultVehicle, evehicleType]
     
     # setup custom routes
     ROUTES = [
-        create_route("r_north_south", [e40, e9], prob=0.1), # NW-SE
-        create_route("pa_north", [e40, e40r], r_type="parking", prob=0.6, parks=PARKING_AREAS_NORTH),
-        create_route("pa_south", [e6, e6r], r_type="parking", prob=0.3, parks=PARKING_AREAS_NORTH) # SW-SW
+        # create_route("r_north_south", [e40, e9], prob=0.6), # NW-SE
+        create_route("pa_south", [e6, e6r], r_type="parking", prob=0.4, parks=PARKING_AREAS_SOUTH) # SW-SW
     ]
-    # TODO fix conflicting park routes
-    # TODO semaphores
     
     ROUTE_PROBS = [x["prob"] for x in ROUTES]
     if None in ROUTE_PROBS or sum(ROUTE_PROBS) != 1:
@@ -82,34 +74,33 @@ def run(traSMAPy: TraSMAPy):
     traSMAPy.publicServices.createFleet(
         "ne-center", b_route1, busType, [ScheduledStop(x, 20) for x in bus_stops_r1], period=200, end=4000
     )
-    # TODO bus lanes (priority or disallow)
-
-        
+    
     vs_parks = []
     for i in range(0, 800):
         route = random.choices(ROUTES, weights=ROUTE_PROBS, k=1)[0]
         
         v = traSMAPy.users.createVehicle(f"v{i}", route["route"], 
                                             random.choices(V_TYPES, weights=(80, 20), k=1)[0])
-
         if route["type"] == "parking":
             park = random.choice(route["park_areas"])
             v.via = [park.lane.parentEdge.id]
             vs_parks.append((v, park))
-
-    while traSMAPy.minExpectedNumber > 0:        
-        if len(vs_parks) > 0 and traSMAPy.step > 10:
-            try:
-                if not vs_parks[0][0].isPending(): # very expensive
-                    vs_parks[0][0].stopFor(vs_parks[0][1], random.randint(400, 600), 
-                                               stopParams=[StopType.PARKING])
-                    vs_parks.pop(0)
-            except Exception as e:
-                print(e)
+    
+    spawned = False
+    while traSMAPy.minExpectedNumber > 0: 
+        try:
+            if len(vs_parks) > 0 and spawned:
+                spawned = False
+                vs_parks[0][0].stopFor(vs_parks[0][1], random.randint(400, 600), 
+                                                stopParams=[StopType.PARKING, StopType.PARKING_AREA])
                 vs_parks.pop(0)
-                pass
+                continue
+            
+            spawned = not vs_parks[0][0].isPending()
+        except Exception as e:
+            print(e)
+            pass
         traSMAPy.doSimulationStep()
-
     traSMAPy.closeSimulation()
 
 
