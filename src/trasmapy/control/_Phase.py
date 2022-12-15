5a3fb88038933c8b4af319907a7f7af561e2e829
Ana Barros <up201806593@fe.up.pt>
2022-12-15 23:14:04 +0000

    Add: phase getters

--- src/trasmapy/control/_Phase.py
+++ src/trasmapy/control/_Phase.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python
 
 from traci._trafficlight import Phase
-from trasmapy.control import SignalColor
+from trasmapy.control.SignalColor import SignalColor
 
 
 class Phase(Phase):
-    def __init__(self, duration, state, minDur=-1, maxDur=-1, next=tuple(), name="") -> None:
+    def __init__(self, duration: int, state: str, minDur: int = -1, maxDur: int = -1, next=tuple(), name: str = "") -> None:
         super().__init__(duration, state, minDur, maxDur, next, name)
 
     @property
-    def state(self) -> str:
-        return SignalColor(self.state).name
+    def duration(self) -> int:
+        """Returns the phase's duration. (s)"""
+        return self.duration
+
+    @property
+    def state(self) -> list[SignalColor]:
+        """Returns the phase's state."""
+        return self.state
+
+    @property
+    def minDuration(self) -> int:
+        """Returns minimum duration (s). Minimum duration applies only to actuated traffic lights. The value is -1 if it does not apply."""
+        return self.minDuration
+
+    @property
+    def maxDuration(self) -> int:
+        """Returns maximum duration (s). Maximum duration applies only to actuated traffic lights. The value is -1 if it does not apply."""
+        return self.maxDur
+
+    @property
+    def next(self):
+        """Returns next."""
+        return self.next
+
+    @property
+    def name(self) -> str:
+        """Returns the phase's name."""
+        return self.name
