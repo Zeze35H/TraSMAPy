c50ecb231ac1938d48869a4391b99538ae5352ca
Ivo Saavedra <ivo_saavedra@outlook.com>
2022-12-15 21:55:56 +0000

    Fix: replaced southern intersection with cloverleaf

--- simulation/simple_uvar/runner.py
+++ simulation/simple_uvar/runner.py
@@ -34,7 +34,7 @@ def run(traSMAPy: TraSMAPy):
     # get edges
     e40 = traSMAPy.network.getEdge("E40")
     e40r = traSMAPy.network.getEdge("-E40")
-    e9 = traSMAPy.network.getEdge("E9")
+    e19 = traSMAPy.network.getEdge("E19.70")
     
     e41 = traSMAPy.network.getEdge("E41")
     e41a = traSMAPy.network.getEdge("-E41.28")
@@ -45,14 +45,6 @@ def run(traSMAPy: TraSMAPy):
     # forbid access to non eletric vehicles
     e41.setDisallowed([defaultVehicle.vehicleClass])
     e41a.setDisallowed([defaultVehicle.vehicleClass])
-    
-    # Detectors
-    # traSMAPy.network.getDetector("e")
-    sw_in0 = traSMAPy.network.getDetector("sw_in0")
-    sw_in1 = traSMAPy.network.getDetector("sw_in1")
-    
-    sw_out0 = traSMAPy.network.getDetector("sw_out0")
-    sw_out1 = traSMAPy.network.getDetector("sw_out1")
 
     queryStr = "network/edges[self.id == 'E40']/vehicleCount"
     traSMAPy.registerQuery("north_in_ve_count", queryStr)
@@ -64,15 +56,6 @@ def run(traSMAPy: TraSMAPy):
     # # main_det = traSMAPy.network.getDetector("main_detector")
     # traSMAPy.users.vehicles.
     # print(main_det.__dict__)
-    
-    def process_in(x):
-        for v in x:
-            vehicle = traSMAPy.users.getVehicle(v)
-            # print(vehicle)
-            # print(dir(vehicle))
-            # print(vehicle.__dict__)
-    
-    sw_in0.listen(process_in)
 
 
     PARKING_AREAS_SOUTH = [traSMAPy.network.getStop(f'pa_sw{x}') for x in range(7)]
@@ -81,7 +64,7 @@ def run(traSMAPy: TraSMAPy):
     
     # setup custom routes
     ROUTES = [
-        create_route("r_north_south", [e40, e9], prob=0.6), # NW-SE
+        create_route("r_north_south", [e40, e19], prob=0.6), # NW-SE
         create_route("pa_south", [e6, e6r], r_type="parking", prob=0.4, parks=PARKING_AREAS_SOUTH) # SW-SW
     ]
     
@@ -94,13 +77,13 @@ def run(traSMAPy: TraSMAPy):
     bus_stops_r0 = [traSMAPy.network.getStop(f"bs_{i}") for i in range(8)]
     b_route0 = traSMAPy.users.getRoute("bus_se_center")
     traSMAPy.publicServices.createFleet(
-        "sw-ne", b_route0, busType, [ScheduledStop(x, 20) for x in bus_stops_r0], period=200, end=4000
+        "sw-ne", b_route0, busType, [ScheduledStop(x, 20) for x in bus_stops_r0], period=200, end=3000
     )
 
     bus_stops_r1 = [traSMAPy.network.getStop(f"bs_ne{i}") for i in range(8)]
     b_route1 = traSMAPy.users.getRoute("bus_ne_center")
     traSMAPy.publicServices.createFleet(
-        "ne-center", b_route1, busType, [ScheduledStop(x, 20) for x in bus_stops_r1], period=200, end=4000
+        "ne-center", b_route1, busType, [ScheduledStop(x, 20) for x in bus_stops_r1], period=200, end=3000
     )
     
     vs_parks = []
@@ -132,7 +115,7 @@ def run(traSMAPy: TraSMAPy):
         traSMAPy.doSimulationStep()
     
     # traSMAPy.users.vehicles.count
-    print(traSMAPy.collectedStatistics[100])
+    print(traSMAPy.collectedStatistics)
     # print(traSMAPy.query(queryStr))
     traSMAPy.closeSimulation()
 
