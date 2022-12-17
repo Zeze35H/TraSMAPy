aa1fd16e26c4532c03deb7700bfc87923b4b3a07
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-12-17 19:28:09 +0000

    Change root file

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ extensions = ['sphinx.ext.autodoc']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-root_doc = 'api'
+root_doc = 'index'
 
 
 # -- Options for HTML output -------------------------------------------------
